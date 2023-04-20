f767fabe5c4ec82464866506b6c2b3f2dd5d401d
José David Martín <joseda.martin@gmail.com>
2023-04-20 10:28:14 +0200

    Update MainConfusionMatrix.scala

--- src/main/scala/es/us/MainConfusionMatrix.scala
+++ src/main/scala/es/us/MainConfusionMatrix.scala
@@ -11,7 +11,6 @@ import org.apache.spark.sql.types.IntegerType
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.monotonically_increasing_id
 
-
 object MainConfusionMatrix {
   def main(args: Array[String]): Unit = {
 
